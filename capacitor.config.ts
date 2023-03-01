1b7da1edacc0b888845ddb7618a796af242b6168
Serin <gabrielmarianoofficial@gmail.com>
2023-03-01 23:40:24 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'super-bikes-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
